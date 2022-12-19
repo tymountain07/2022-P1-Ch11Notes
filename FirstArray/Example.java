6127fd37cf25babe5c68422d41f05c53e405537b
14261 <t#@vtsd.com>
2022-12-19 07:57:48 -0500

    Chapter 11 Notes

---
+++ FirstArray/Example.java
@@ -0,0 +1,16 @@
+package FirstArray;
+
+public class Example {
+    public static void main(String args[]){
+
+        // Arrays store one datatype
+        int numberOfChristmasGifts[];
+        numberOfChristmasGifts = new int[2];
+
+        numberOfChristmasGifts[0] = 7;
+        numberOfChristmasGifts[1] = 3;
+
+        System.out.println(numberOfChristmasGifts[0]);
+
+    }
+}
---
+++ Temp/Temp.java
@@ -0,0 +1,23 @@
+package Temp;
+
+public class Temp {
+    public static void main(String args[]) {
+
+        // Arrays store one datatype
+        int numberOfChristmasGifts[];
+        numberOfChristmasGifts = new int[2];
+
+        numberOfChristmasGifts[0] = 7;
+        numberOfChristmasGifts[1] = 3;
+
+        System.out.println(numberOfChristmasGifts[0]);
+        System.out.println(numberOfChristmasGifts[1]);
+
+        numberOfChristmasGifts[0] = 42;
+        numberOfChristmasGifts[1] = 6;
+
+        System.out.println(numberOfChristmasGifts[0]);
+        System.out.println(numberOfChristmasGifts[1]);
+
+    }
+}
